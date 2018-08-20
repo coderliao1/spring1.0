e4d19a92838356582067722648fe8740536ed8b0
coderliao1 <312905249@qq.com>
2018-08-20 23:02:35 +0800

    auto_scan_1

--- src/test/java/org/litespring/test/AllTests.java
+++ src/test/java/org/litespring/test/AllTests.java
@@ -4,8 +4,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.litespring.test.v1.V1ALLTests;
 import org.litespring.test.v2.V2ALLTests;
+import org.litespring.test.v3.V3ALLTests;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({V1ALLTests.class,V2ALLTests.class})
+@Suite.SuiteClasses({V1ALLTests.class,V2ALLTests.class,V3ALLTests.class})
 public class AllTests {
 }
